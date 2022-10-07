21feb2c9644c98e8e82ca54a861362a950a8d12f
loque18 <madridx2010@gmail.com>
2022-10-06 20:57:10 -0500

    feat: added mint details

--- pages/howto/index.js
+++ pages/howto/index.js
@@ -3,7 +3,40 @@ import { getLayout as getMainLayout } from 'src/layouts/main';
 import { getLayout as getSectionLayout } from 'src/layouts/section';
 
 const HowtoPage = () => {
-    return <div className="" style={{ minHeight: '100vh' }}></div>;
+    return (
+        <div className="" style={{ minHeight: '100vh' }}>
+            <table className="table has-bg-burgundy has-text-gold">
+                <thead>
+                    <tr className="has-text-burgundy">
+                        <th>
+                            <h1>Wins</h1>
+                        </th>
+
+                        <th>
+                            <h1>Draws</h1>
+                        </th>
+
+                        <th>
+                            <h1>Loses</h1>
+                        </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <h1>+3</h1>
+                        </td>
+                        <td>
+                            <h1>+1</h1>
+                        </td>
+                        <td>
+                            <h1>0</h1>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 HowtoPage.getLayout = page => getPageTitleLayout(getMainLayout(getSectionLayout(page, 'How to')), 'How to');
